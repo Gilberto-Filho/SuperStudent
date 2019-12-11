839a4ca359f3194bc0c95ed65272573272afdc71
F1L1P3P4UL1N0 <fpb@discente.ifpe.edu.br>
2019-12-10 21:25:35 -0300

    Criação do menu principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { TarefasComponent } from './tarefas/tarefas.component';
+import { MateriasComponent } from './materias/materias.component';
+import { MetodosComponent } from './metodos/metodos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    TarefasComponent,
+    MateriasComponent,
+    MetodosComponent
   ],
   imports: [
     BrowserModule
